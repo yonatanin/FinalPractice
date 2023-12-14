8a4668ce3df1cf4eb0b0723248e6c825eeee8ac4
yonatanin <yoyocnolasco@s.sfusd.edu>
2023-12-14 15:41:30 -0800

    Sound Effects added

--- Player.gd
+++ Player.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
 @export var Speed = 400
+@onready var b_gmusic = $"../BGmusic"
+@onready var enemy_hit = $"../EnemyHit"
 
 func _process(delta):
 	var velocity = Vector2.ZERO
@@ -21,3 +23,6 @@ func _process(delta):
 
 func _on_body_entered(body):
 	hide()
+	enemy_hit.play()
+	b_gmusic.stop()
+	
